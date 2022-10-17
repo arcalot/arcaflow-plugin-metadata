e0bb00f34a8d717133e7dc6616c4ad3c818ab808
mleader <mleader@redhat.com>
2022-10-17 14:03:32 -0400

    fix code style

--- metadata_plugin.py
+++ metadata_plugin.py
@@ -13,13 +13,13 @@ from ansible.vars.manager import VariableManager
 from ansible.utils.unsafe_proxy import AnsibleUnsafeText
 from ansible import context
 
+
 @dataclass
 class InputParams:
     """
     This is the data structure for the input parameters of the step defined
     below.
     """
-    pass
 
 
 @dataclass
@@ -27,6 +27,7 @@ class SuccessOutput:
     """
     This is the output data structure for the success case.
     """
+
     metadata: typing.Any = field()
 
 
@@ -56,21 +57,22 @@ def collect_metadata(
     :return: the string identifying which output it is, as well the output
         structure
     """
-    host = 'localhost'
-    
-    context.CLIARGS = ImmutableDict(connection='local',
-        #module_path=['/to/mymodules', '/usr/share/ansible'],
+    host = "localhost"
+
+    context.CLIARGS = ImmutableDict(
+        connection="local",
+        # module_path=['/to/mymodules', '/usr/share/ansible'],
         forks=5,
         become=None,
         become_method=None,
         become_user=None,
         check=False,
         diff=False,
-        verbosity=0
+        verbosity=0,
     )
 
     loader = DataLoader()
-    inventory = InventoryManager(loader=loader, sources=host + ',')
+    inventory = InventoryManager(loader=loader, sources=host + ",")
     variable_manager = VariableManager(loader=loader, inventory=inventory)
 
     # Must be initialized before play.load()
@@ -79,7 +81,7 @@ def collect_metadata(
         variable_manager=variable_manager,
         loader=loader,
         passwords=dict(),
-        #stdout_callback=results_callback,  # Use our custom callback instead of the ``default`` callback plugin, which prints to stdout
+        # stdout_callback=results_callback,  # Use our custom callback instead of the ``default`` callback plugin, which prints to stdout
     )
 
     play_source = dict(
@@ -97,28 +99,36 @@ def collect_metadata(
         tqm.cleanup()
         if loader:
             loader.cleanup_all_tmp_files()
-    
+
     vars = variable_manager.get_vars(play=play, include_hostvars=True)
     host_ansible_facts = vars["hostvars"][host]["ansible_facts"]
     # Convert to dict
-    output =  convert_to_supported_type(host_ansible_facts)
+    output = convert_to_supported_type(host_ansible_facts)
 
     return "success", SuccessOutput(output)
 
-def  convert_to_supported_type(ansible_value):
+
+def convert_to_supported_type(ansible_value):
     type_of_val = type(ansible_value)
     if type_of_val == list:
         new_list = []
         for i in ansible_value:
-            new_list.append( convert_to_supported_type(i))
+            new_list.append(convert_to_supported_type(i))
         return new_list
     if type_of_val == dict:
         result = {}
         for k in ansible_value:
-            result[ convert_to_supported_type(k)] =  convert_to_supported_type(ansible_value[k])
+            result[convert_to_supported_type(k)] = convert_to_supported_type(
+                ansible_value[k]
+            )
         return result
-    if type_of_val == float or type_of_val == int or type_of_val == str or \
-        type_of_val == bool or type_of_val == type(None):
+    if (
+        type_of_val == float
+        or type_of_val == int
+        or type_of_val == str
+        or type_of_val == bool
+        or type_of_val == type(None)
+    ):
         return ansible_value
     elif type_of_val == AnsibleUnsafeText:
         return str(ansible_value)
